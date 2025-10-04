adb37271923917e59aa4b53179dbb6c613ffc73d
sqatm <sqatmah@gmail.com>
2025-10-04 22:34:37 +0300

    order blog posts by post_date (latest first)

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -16,7 +16,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    #ordering = ['-id'] # to show the latest post first we use -id
+    ordering = ['-post_date'] 
+   # ordering = ['-id'] # to show the latest post first we use -id
 
 
 class ArticleDetailView(DetailView):
